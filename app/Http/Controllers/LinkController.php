637afafb6f322cab480245a04de8a3d568ddef46
chawrona <dawidchawrona@gmail.com>
2025-02-11 03:22:21 +0100

    Configured Link Controller

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -2,6 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Link;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
 class LinkController
 {
+    public function index() {
+        $links = Link::all();
+        return response()->json($links);
+    }
+
+    public function store(Request $request) {
+
+        $validated = $request->validate([
+            'original_url' => 'required|url',
+        ]);
+
+        $link = Link::where('original_url', $validated['original_url'])->first();
+
+        if ($link) {
+            $link->touch();
+        } else {
+            $link = Link::create([
+                'original_url' => $validated['original_url'],
+                'shortened_url' => substr(md5(uniqid()), 0, 6),
+            ]);
+        }
+
+        $link->save();
+
+        return Redirect::to("/links");
+    }
+
+    public function redirect($shortened_url) {
+
+        $link = Link::where('shortened_url', $shortened_url)->first();
+
+        if (!$link) return Redirect::to("/notfound");
+
+        return Redirect::to($link->original_url);
+    }
 }
